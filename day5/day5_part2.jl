8660739e35c6d6d75422f4888850fa59b6a76edb
Valter Schutz <valterschutz@proton.me>
2023-12-08 15:33:57 +0100

    day 5

--- day5/day5_part2.jl
+++ day5/day5_part2.jl
@@ -2,45 +2,75 @@ using PrettyPrinting
 using ProgressBars
 # using StaticArrays
 using DataStructures
+using ProgressMeter
 
-"""
-    parse_map(str)
-
-Converts mapping such as
-    50 98 2
-    52 50 48
-into a mapping of Function type.
-"""
 function parse_map(str)
     thing = [[parse(Int,cap) for cap in m.captures] for m in eachmatch(r"^(\d+) (\d+) (\d+)$"m, str)]
-    d = Dict((src_range_start:(src_range_start+range_length-1), dest_range_start:(dest_range_start+range_length-1)) for (dest_range_start, src_range_start, range_length) in thing)
-    return function(src)
-        for (dest_range_start, src_range_start, range_length) in thing
-            if src in src_range_start:(src_range_start+range_length-1)
-                return dest_range_start + (src-src_range_start)
-            end
-        end
-        return src
-    end
+    return Dict((src_range_start:(src_range_start+range_length-1), dest_range_start:(dest_range_start+range_length-1)) for (dest_range_start, src_range_start, range_length) in thing)
 end
 
-function parse_seeds(str)
+function parse_seed_ranges(str)
     g = (parse.(Int,m.captures) for m in eachmatch(r"(\d+) (\d+)", str))
-    return (i for (range_start, range_length) in g for i in range_start:(range_start+range_length-1))
+    return [range_start:(range_start+range_length-1) for (range_start, range_length) in g]
+end
+
+function merge_ranges(ranges)
+    if isempty(ranges)
+        return []
+    end
+
+    # Sort the ranges based on their start values
+    sorted_ranges = sort(ranges, by=x->x.start)
+
+    merged_ranges = [sorted_ranges[1]]
+
+    # for i in 2:length(sorted_ranges)
+    for current_range in sorted_ranges[2:end]
+        # current_range = sorted_ranges[i]
+        last_merged_range = merged_ranges[end]
+
+        # Check for overlap
+        if current_range.start <= last_merged_range.stop
+            # Merge overlapping ranges
+            merged_ranges[end] = last_merged_range.start:max(current_range.stop, last_merged_range.stop)
+        else
+            # Add non-overlapping range to the result
+            push!(merged_ranges, current_range)
+        end
+    end
+
+    return merged_ranges
+end
+
+function get_src(mapping, dest)
+    for (src_range, dest_range) in mapping
+        if dest in dest_range
+            return src_range.start + (dest - dest_range.start)
+        end
+    end
+    return dest
 end
 
 function main()
     file_contents = read("day5_input.txt", String)
     v = split(file_contents, "\n\n")
 
-    seeds = parse_seeds(v[1])
-    println("seeds parsed")
-    maps = parse_map.(v[2:end])
-    println("maps parsed")
+    seed_ranges = merge_ranges(parse_seed_ranges(v[1]))
 
-    # For each seed, pass it through the maps
-    lowest_location = minimum(foldl((acc,f) -> f.(acc), maps, init=seeds))
+    maps = parse_map.(v[2:end])
+
+    # The lowest location could be any UInt32...
+    local lowest_location
+    for location in 1:typemax(UInt32)
+        seed_candidate = foldl((acc,d) -> get_src(d,acc), reverse(maps), init=location)
+        if any(seed_candidate in seed_range for seed_range in seed_ranges)
+            lowest_location = location
+            break
+        end
+    end
     println("Lowest location: $lowest_location")
 end
 
-main()
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
